e784777bb252c77970a79b8a50e97086359aa81a
Aaron Pepper <pepdog@gmail.com>
2025-02-28 05:44:08 -0500

    basic mobile menu setup (#27)

--- src/sites/tari-dot-com/ui/Header/MobileMenu/styles.ts
+++ src/sites/tari-dot-com/ui/Header/MobileMenu/styles.ts
@@ -9,3 +9,27 @@ export const Wrapper = styled.div`
         display: block;
     }
 `;
+
+export const MenuTrigger = styled.button`
+    width: 50px;
+    height: 50px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    transform: translateX(10px);
+    cursor: pointer;
+`;
+
+export const MenuWrapper = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 80;
+    background-color: #000;
+    width: 100%;
+    height: 100dvh;
+
+    display: none;
+`;
