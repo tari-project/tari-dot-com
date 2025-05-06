c78c55fbcabc42a40a840ff32a11a0bc0a62d451
Juan De Luca <juan.f.d.luca@gmail.com>
2025-05-06 08:32:59 +0200

    feat: Update Banner to show Mainnet is live
    Updated the Banner component to reflect that Tari Mainnet is now live. Removed the previous Testnet announcement and the countdown to the Mainnet launch date.

--- src/sites/tari-dot-com/ui/Banner/Banner.tsx
+++ src/sites/tari-dot-com/ui/Banner/Banner.tsx
@@ -7,8 +7,7 @@ export default function Banner() {
         <Wrapper>
             <Holder>
                 <Text>
-                    Tari is in Testnet. <GradientText>Mainnet launches May 6, 2025!</GradientText>
-                    {/*}Tari Mainnet <GradientText>is live</GradientText>{*/}
+                    Tari Mainnet <GradientText>is live</GradientText>
                 </Text>
             </Holder>
         </Wrapper>
