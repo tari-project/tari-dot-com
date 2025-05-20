6c1ea43e81bc1fff4a4f07f91a6b331856c5ddc7
Aaron Pepper <pepdog@gmail.com>
2025-05-20 18:12:33 +0900

    page title fix

--- src/app/(exchange)/exchange/[name]/layout.tsx
+++ src/app/(exchange)/exchange/[name]/layout.tsx
@@ -1,28 +1,4 @@
 import LayoutExchange from '@/ui-shared/layouts/Layout/LayoutExchange';
-import { exchangeData } from './page';
-
-export const generateMetadata = async ({ params }: { params: { name: string } }) => {
-    const exchange = exchangeData[params.name] || { name: params.name };
-
-    const metadata = {
-        title: `Tari x ${exchange.name}`,
-        description:
-            'Tari is the L1 protocol powered by you. Proof of work and an ingenious app platform to put all of its power in your hands.',
-        icons: [{ url: 'https://tari.com/favicon.png?v=1', type: 'image/png' }],
-        openGraph: {
-            images: [
-                {
-                    url: 'https://tari.com/tari-og.png?v=2',
-                    width: 1200,
-                    height: 630,
-                    alt: 'Tari',
-                },
-            ],
-        },
-    };
-
-    return metadata;
-};
 
 export default function ExchangeLayout({ children }: { children: React.ReactNode }) {
     return <LayoutExchange>{children}</LayoutExchange>;
--- src/app/(exchange)/exchange/[name]/page.tsx
+++ src/app/(exchange)/exchange/[name]/page.tsx
@@ -58,6 +58,27 @@ export const exchangeData: Record<string, Exchange> = {
     },
 };
 
+export const generateMetadata = async ({ params }: { params: { name: string } }) => {
+    const exchange = exchangeData[params.name] || { name: params.name };
+
+    return {
+        title: `Tari x ${exchange.name}`,
+        description:
+            'Tari is the L1 protocol powered by you. Proof of work and an ingenious app platform to put all of its power in your hands.',
+        icons: [{ url: 'https://tari.com/favicon.png?v=1', type: 'image/png' }],
+        openGraph: {
+            images: [
+                {
+                    url: 'https://tari.com/tari-og.png?v=2',
+                    width: 1200,
+                    height: 630,
+                    alt: 'Tari',
+                },
+            ],
+        },
+    };
+};
+
 export default function Page({ params }: ExchangePageProps) {
     const { name } = params;
     const exchange = exchangeData[name as keyof typeof exchangeData];
